32519e9d49484ac6264a6243ba0517f765c284ea
nagatharun0000 <nagatharun0000@gmail.com>
2025-07-04 10:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=3000)
