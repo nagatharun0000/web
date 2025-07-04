40f1d9aa097e5208e859de4435b20f2732f1e579
nagatharun0000 <nagatharun0000@gmail.com>
2025-07-04 10:26:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,8 +105,10 @@ def edit_note(note_id):
     return render_template('edit_note.html', note=note)
 
 # Run the app
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Railway!"
